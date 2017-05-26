808acd8b2ef4f3e4b5aa6aa873c79684c46e05aa
maxfqs <maxfqs@gmail.com>
2017-05-26 17:26:38 +0200

    add: $.find override

--- src/client/jquery.ts
+++ src/client/jquery.ts
@@ -3,3 +3,15 @@ import * as jquery from "jquery"
 
 declare const $:JQueryStatic
 export default $
+
+
+// Small override to log an error if nothing is find
+let find = $.fn.find;
+$.fn.find = function() {
+    let result = find.apply(this, arguments);
+    if (result.length == 0) {
+        console.error("Jquery: cannot find " + arguments[0]);
+    }
+
+    return result;
+}
