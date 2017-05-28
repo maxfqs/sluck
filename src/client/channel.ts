5d7f2e56081c6f567e1b7ebd0d4551909d6d9f3e
maxfqs <maxfqs@gmail.com>
2017-05-28 11:48:40 +0200

    add: events to Channel

--- src/client/channel.ts
+++ src/client/channel.ts
@@ -1,51 +1,88 @@
 import $ from "../client/jquery"
 import {ChannelListItem} from "../client/left-panel"
 import * as chat from "../client/chat"
+import EventEmitter from "./event-emitter"
 import MessageContainer from "../client/message-container"
 import {Shema} from "../interface/database"
 
-/** Currently selected channel */
-let selected: Channel = null;
+
+const events = new EventEmitter();
+
+
+/**
+* Channel events
+* key => event's name
+* value => args send type
+*/
+interface Events {
+    "open": Channel
+    "close": Channel
+}
 
 
 export default class Channel {
-    container: MessageContainer
-    data: Shema<"channels">
-    item: ChannelListItem
+    /** Currently selected channel */
+    private static selected: Channel = null;
+
+    private container: MessageContainer
+    private data: Shema<"channels">
+    private item: ChannelListItem
 
     constructor(channel: Shema<"channels">) {
-        let self = this;
-
         this.data = channel;
 
         this.container = new MessageContainer;
-
         this.item = new ChannelListItem(channel.name);
+
+        let self = this;
         this.item.$.on("click", function() {
             self.open();
         })
     }
 
+    /** Static - Register an event handler */
+    static on <E extends keyof Events> (e: E, cb: (args: Events[E]) => void) {
+        events.on(e, cb);
+    }
+
+    /** Static - Trigger the event */
+    static emit <E extends keyof Events> (e: E, args: Events[E]) {
+        events.emit(e, args);
+    }
+
+
+    /** Static - Return the currently selected channel */
+    static getSelected() {
+        return Channel.selected;
+    }
+
     /** Open the channel */
     open() {
-        if (selected == this) {
+        if (Channel.selected == this) {
             return false;
         }
 
-        if (selected != null) {
-            selected.close();
+        if (Channel.selected != null) {
+            Channel.selected.close();
         }
 
         chat.openChannel(this.data.name);
         this.item.setActive();
         this.container.open();
 
-        selected = this;
+        Channel.selected = this;
+        Channel.emit("open", this);
     }
 
     /** Close the channel */
     close() {
         this.item.setInactive();
         this.container.close();
+        Channel.emit("close", this);
+    }
+
+    /** Get channel ID */
+    getID() {
+        return this.data.id;
     }
 }
