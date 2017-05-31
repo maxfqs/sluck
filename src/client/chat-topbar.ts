70421c8476c2d3426d2d49ac4e5755e71d366d97
maxfqs <maxfqs@gmail.com>
2017-05-31 23:51:13 +0200

    add: users button on chat topbar

--- src/client/chat-topbar.ts
+++ src/client/chat-topbar.ts
@@ -2,10 +2,18 @@ import $ from "../client/jquery"
 import Channel from "../client/channel"
 
 const $topbar = $("#app").find("#chat-topbar");
+const $usersList = $("#app").find("#user-list");
 const $channelName = $topbar.find("#channel-name");
+const $users = $topbar.find("#users");
 
 
 // Update the channel name
 Channel.on("open", function(chan) {
     $channelName.text(chan.getName());
+})
+
+// Open / Close the users list panel
+$users.on("click", function() {
+    $users.toggleClass("selected");
+    $usersList.toggleClass("hide");
 })
